596e76752b4a14cccba46d95f191e504601b8530
Benedek Balint Toth <benedek.toth@bookrkids.hu>
2024-03-18 10:20:18 +0100

    upload 2024-03-18 10-20

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var isAccessibilityModeOn = true;
 var isNoGameModeOn = false;
 var myProjectName = "cet";
 
-setTimeout(SetUpMode, 4500);
+setTimeout(SetUpMode, 5000);
 
 var spinnerLoaderForMenu = document.getElementById("theSpinnerLoaderForMenu");
 var spinnerLoader = document.getElementById("theSpinnerLoader");
