5b1a7a378f08ea7b0d9ec81bf044c8679dc09c5f
Benedek Balint Toth <benedek.toth@bookrkids.hu>
2024-03-18 10:40:56 +0100

    upload 2024-03-18 10-40

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var isAccessibilityModeOn = true;
 var isNoGameModeOn = false;
 var myProjectName = "cet";
 
-// setTimeout(SetUpMode, 5500);
-setInterval(SetUpMode, 1000);
+setTimeout(SetUpMode, 5500);
+// setInterval(SetUpMode, 1000);
 
 var spinnerLoaderForMenu = document.getElementById("theSpinnerLoaderForMenu");
 var spinnerLoader = document.getElementById("theSpinnerLoader");
@@ -267,6 +267,8 @@ function SetUpMode()
 
     dispatchAccessabilityModeUnityMessage(isAccessibilityModeOn);
     dispatchNoGameModeUnityMessage(isNoGameModeOn);
+
+    setInterval(SetUpMode, 1000);
 }
 
 function BookDataRecived(jsonData, isAllowedToSeePaidBooks)
